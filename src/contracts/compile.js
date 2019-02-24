8cf401f22a91f45cf1195b8518aac4292a37483a
Diego Braga <dsbrgg@gmail.com>
2019-02-24 22:38:51 +0000

    chore: unlock default account in chain, wip for deploying contracts

--- src/contracts/compile.js
+++ src/contracts/compile.js
@@ -1,19 +1,44 @@
 const fs = require('fs')
 const path = require('path')
 const solc = require('solc')
+const Web3 = require('web3')
+
+const web3 = new Web3('ws://localhost:8546')
+web3.setProvider('ws://localhost:8546')
 
 const contractsPath = path.resolve(__dirname, 'contracts')
-const contractsDir = fs.readdirSync(contractsPath)
 
-contractsDir.forEach(contract => {
-  const source = fs.readFileSync(`${contractsPath}/${contract}`, 'utf8')
-  const compiledContract = solc.compile(source, 1)
+const gas = 50000000
+const from = '0x6d36bea3b2a5f6c6d6f046e89c34d040bb29cc6d'
 
-  const abi = compiledContract.contracts[':Greeter'].interface
-  const bytecode = compiledContract.contracts[':Greeter'].bytecode
+const compile = () => 
+  fs.readdirSync(contractsPath)
+    .reduce((obj, contract) => {
+      const contractFilePath = `${contractsPath}/${contract}`
 
-  console.log(abi)
-  console.log(bytecode)
-})
+      const source = fs.readFileSync(contractFilePath, 'utf8')
+      const { contracts } = solc.compile(source, 1)
 
-// TODO: SEND ABI AND BYTECODE TO SPECIFIC FOLDER
+      return { ...obj, ...contracts }
+    }, {})
+
+const deploy = async (contracts) => {
+  const toDeploy = []
+
+  Object.keys(contracts).forEach(key => {
+    const abi = JSON.parse(contracts[key].interface)
+    const data = `0x${contracts[key].bytecode}`
+
+    const Contract = new web3.eth.Contract(abi, { from, data })
+    
+    toDeploy.push(
+      Contract.deploy({ data, arguments: [] }).send({ from, gas })
+    )
+  })
+
+  return Promise.all(toDeploy)
+}
+
+;(async function() {
+  await deploy(compile())
+})()
--- src/contracts/contracts/Test.sol
+++ src/contracts/contracts/Test.sol
@@ -1,17 +1,7 @@
 pragma solidity ^0.4.24;
 
-contract Mortal {
-  /* Define variable owner of the type address */
-  address owner;
 
-  /* This constructor is executed at initialization and sets the owner of the contract */
-  constructor() public { owner = msg.sender; }
-
-  /* Function to recover the funds on the contract */
-  function kill() public { if (msg.sender == owner) selfdestruct(msg.sender); }
-}
-
-contract Greeter is Mortal {
+contract Greeter {
   /* Define variable greeting of the type string */
   string greeting;
 
