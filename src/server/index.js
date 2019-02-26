53f67160e3da04ae0f15bf3c1b4de7ff1275d9c2
Diego Braga <dsbrgg@gmail.com>
2019-02-26 10:55:52 +0000

    chore: initial routing and server listening

--- src/server/index.js
+++ src/server/index.js
@@ -1,12 +1,17 @@
 'use strict'
 
+const Koa = require('koa')
 const Web3 = require('web3')
 
 const web3 = new Web3('ws://localhost:8546')
 web3.setProvider('ws://localhost:8546')
 
-;(async function() {
-  const ola = await Greeter.methods.greet().call()
+const { setupContracts } = require('./middleware')
 
-  console.log(ola)
-})()
+const app = new Koa()
+
+app.use(setupContracts(web3))
+
+app.listen(8080, () => {
+  console.info('Listening on port 8080')
+})
---
+++ src/server/router/index.js
@@ -0,0 +1,18 @@
+'use strict'
+
+const Router = require('koa-router')
+const graphqlHTTP = require('koa-graphql')
+
+const { schema } = require('../graphql')
+
+const router = Router()
+router.all('/api/graphql', graphqlHTTP({
+	schema,
+	// https://github.com/graphql/express-graphql/issues/427
+	// https://github.com/apollographql/graphql-tools/issues/480
+	formatError: (err) => err.originalError
+		? err.originalError.errors[0].originalError
+		: err
+}))
+
+module.exports = router
